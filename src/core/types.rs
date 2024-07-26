bf499ed3229c4d13dc1fbfccee3166df00445aff
sabjorn <info@sabjorn.net>
2024-07-25 17:37:49 -0700

    implement function for calculating totals of a Vec of transcations

--- src/core/types.rs
+++ src/core/types.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
-//use chrono::{DateTime, Utc};
 
 pub type AccountId = i64;
 pub type TranscationId = i64;
 pub type BlockId = i64;
+type Transactions = Vec<Transaction>;
 
 pub struct Transaction {
     id: TranscationId,
@@ -12,24 +12,85 @@ pub struct Transaction {
     amount: f64,
 }
 
-pub struct TransactionList(pub Vec<Transaction>);
+trait TransactionTotal {
+    fn calculate_total(&self, account: AccountId) -> Option<f64>;
+}
 
-impl TransactionList {
-    pub fn new() -> Self {
-        TransactionList(Vec::new())
+impl TransactionTotal for Transactions {
+    fn calculate_total(&self, account: AccountId) -> Option<f64> {
+        let mut sum = 0.;
+        let mut found = false;
+
+        for t in self {
+            if t.to == account {
+                sum += t.amount;
+                found = true;
+            }
+            if t.from == account {
+                sum -= t.amount;
+                found = true;
+            }
+        }
+
+        if found {
+            Some(sum)
+        } else {
+            None
+        }
     }
 }
 
-
 struct Block {
     id: BlockId,
-    //time: DateTime<Utc>,
-    transactions: Vec<Transaction>,
+    transactions: Transactions,
 }
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    fn create_relationships() -> (Collection, Owned) {
-//    }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_calculate_total_for_existing_id(){
+        let transactions: Transactions = vec!(
+            Transaction{ 
+                id: 1,
+                to: 1,
+                from: 2,
+                amount: 2.34
+            },
+            Transaction{ 
+                id: 2,
+                to: 1,
+                from: 2,
+                amount: 10.00
+            });
+
+        let result = transactions.calculate_total(1);
+        assert!(result.is_some());
+        assert_eq!(result, Some(12.34));
+
+        let result = transactions.calculate_total(2);
+        assert!(result.is_some());
+        assert_eq!(result, Some(-12.34));
+    }
+
+    #[test]
+    fn test_calculate_total_no_id_returns_none(){
+        let transactions: Transactions = vec!(
+            Transaction{ 
+                id: 1,
+                to: 1,
+                from: 2,
+                amount: 2.34
+            },
+            Transaction{ 
+                id: 2,
+                to: 1,
+                from: 2,
+                amount: 10.00
+            });
+
+        let result = transactions.calculate_total(3);
+        assert!(result.is_none());
+    }
+}
